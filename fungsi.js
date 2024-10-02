1bcaf8d7c6030d53de6d3f87912b92d9406315cb
Maspin <gustia032@gmail.com>
2024-10-02 14:04:34 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,11 @@
 const add = (a, b) => a + b;
 console.log(add(1,2));
 
-const subtract = (a, b) => a - b;
+const subtract =(a, b) => a - b;
 console.log(subtract(5,2));
 
-const multiply = (a, b) => a * b; 
-console.log(multiply(2, 2));
+const multiplynumber = (a, b) => a * b; 
+console.log(multiplynumber(2, 2));
 
 const divide = (a, b) => a / b;
 console.log = (divide(4, 2));
