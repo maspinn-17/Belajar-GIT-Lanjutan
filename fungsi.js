145969f0f09833067f9d92bc5bcb9e2833ea217c
Maspin <gustia032@gmail.com>
2024-10-02 13:45:45 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,6 +1,8 @@
 const add = (a, b) => a + b;
 console.log(add(1,2));
 
-const subtract =(a, b) => a - b;
+const subtract = (a, b) => a - b;
 console.log(subtract(5,2));
 
+const multiply = (a, b) => a * b; 
+console.log(multiply(2, 2));
